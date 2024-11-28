d1b2ba610fe445d52848e66dd06fa0049a8162ca
Ismail Halili <i.halili99@hotmail.com>
2024-11-28 22:53:08 +0100

    ttest netlifty

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync("./.cert/localhost+2-key.pem"),
+      cert: fs.readFileSync("./.cert/localhost+2.pem"),
+    },
+  },
   plugins: [react()],
-})
+});
